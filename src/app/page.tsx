cce7399d254c09eda01a512542d67fb722b2d57d
美波 <minami@minaminoMacBook-Pro.local>
2024-11-26 21:15:53 +0900

    contentscard commentout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import "../styles/globals.scss";
 import styles from "./page.module.scss";
-import ContentCard from "@/components/ContentCard";
+// import ContentCard from "@/components/ContentCard";
 
 export default function Home() {
   return (
