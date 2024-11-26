34d5661fb07c40bd40166422fab3b7de2e772d8e
美波 <minami@minaminoMacBook-Pro.local>
2024-11-26 20:54:08 +0900

    画像以外の不要箇所削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,22 @@
 import Image from "next/image";
-import '../styles/globals.scss'; // グローバルCSSをここでインポート
+import "../styles/globals.scss";
 import styles from "./page.module.scss";
 import ContentCard from "@/components/ContentCard";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div className={styles.Top__mainWrapper}>
       <main className={styles.main}>
-        <ContentCard/ >
+        <div style={{ position: "relative", width: "100%", height: "600px" }}>
+          <Image
+            src="/background/headerImage.png"
+            alt="Header Image"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+        {/* <ContentCard /> */}
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
