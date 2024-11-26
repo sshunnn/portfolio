b4e49a380ddd6bf512fef96711704d1a7f57c313
美波 <minami@minaminoMacBook-Pro.local>
2024-11-26 20:50:21 +0900

    Header: wip

---
+++ src/components/MainHeader/index.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import styles from "./styles.module.scss";
+
+export default function MainHeader() {
+  return (
+    <header>
+      <div className={styles.MainHeader__wrapper}>
+        <p className={styles.MainHeader__siteName}>Shun's logs</p>
+        <nav className={styles.MainHeader__navigationWrapper}>
+          <a
+            href="https://nextjs.org/learn"
+            className={styles.MainHeader__headerLink}
+          >
+            Detail
+          </a>
+          <a
+            href="https://nextjs.org/docs"
+            className={styles.MainHeader__headerLink}
+          >
+            About
+          </a>
+          <a
+            href="https://nextjs.org/learn"
+            className={styles.MainHeader__headerLink}
+          >
+            Contact
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
+}
