2bc83fdd2bc2a11a109852dced286734f64a333c
美波 <minami@minaminoMacBook-Pro.local>
2024-12-15 20:30:55 +0900

    微修正マークアップ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,35 @@
 import Image from "next/image";
 import "../styles/globals.scss";
 import styles from "./page.module.scss";
-// import ContentCard from "@/components/ContentCard";
 import ContentHeading from "@/components/ContentHeading";
 import ContentCard from "@/components/ContentCard";
 
 export default function Home() {
   return (
     <>
-      <div className={styles.Top__mainWrapper}>
-        <div className={styles.Top__imageWrapper}>
-          <Image
-            src="/background/headerImage.png"
-            alt="Header Image"
-            width={1920}
-            height={600}
-            objectFit="cover"
-            loading="lazy"
-          />
-        </div>
-        <ContentHeading
-          headLevel="h1"
-          text="About my portfolio Detail"
-          className={styles.Top__MainHeading}
+      <div className={styles.Top__imageWrapper}>
+        <Image
+          src="/background/headerImage.png"
+          alt="Header Image"
+          width={1920}
+          height={600}
+          objectFit="cover"
+          loading="lazy"
         />
+        <div className={styles.Top__MainHeading}>
+          <ContentHeading
+            headLevel="h1"
+            text="About my portfolio Detail"
+            className={styles.Top__MainHeading}
+          />
+          <p>
+            特に目的なく作成しているため、特に完成予定などはありません。一旦今までの技術を表現するためにこのページを作成しました。
+          </p>
+        </div>
       </div>
-      <main>
+      {/* <main>
         <ContentCard title="test1" description="test description" />
-      </main>
+      </main> */}
     </>
   );
 }
