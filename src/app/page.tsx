cebdc34540a1b39370cb8408abfd9366e0d891fb
shun <piassy.c0mp@gmail.com>
2025-07-25 01:16:32 +0900

    不要コメント

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,9 @@ import HomePage from "@/views/templates/HomePage";
 import { getHomePageContent } from "@/controllers/contentController";
 
 export default async function Home() {
-  // サーバーサイドでデータを取得
+  // get Server Side
   const { headerTitle, headerDescription, contentItems } = await getHomePageContent();
   
-  // ビューにデータを渡す
   return (
     <HomePage
       headerTitle={headerTitle}
